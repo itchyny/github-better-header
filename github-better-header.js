75c4247739a525b134b7f8e8c35d8e2c0c3d9061
itchyny <itchyny@hatena.ne.jp>
2015-06-20 17:14:52 +0900

    add github-better-header.js

---
+++ github-better-header.js
@@ -0,0 +1,121 @@
+;(function() {
+
+  // Promise object of retrieving a web resource
+  function get(url) {
+    var deferred = Promise.defer();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        deferred.resolve(xhr.response);
+      } else {
+        deferred.reject(new Error(xhr.statusText));
+      }
+    };
+    xhr.onerror = deferred.reject;
+    xhr.send();
+    return deferred.promise;
+  }
+
+  // Promise object of gathering variables
+  function variablesPromise(variables) {
+    var deferred = Promise.defer();
+    var values = {};
+    deferred.promise.set = function(name, value) {
+      values[name] = value;
+      if (Object.keys(values).length === variables.filter(function(v) {
+        return !v.when || values.hasOwnProperty(v.when) && values[v.when];
+      }).length) {
+        deferred.resolve(values);
+      }
+    };
+    return deferred.promise;
+  }
+
+  // Mutation observer witness with the settings injected
+  function witness(setting) {
+    var varpromise = variablesPromise(setting.variables);
+    var htmlpromise = setting.overwrites.map(function(overwrite) {
+      return [ overwrite.fileName, get(chrome.extension.getURL(overwrite.fileName)) ];
+    }).reduce(function (o, v) { o[v[0]] = v[1]; return o; }, {});
+    return function(mutations) {
+      [].forEach.call(mutations, function(mutation) {
+        [].forEach.call(mutation.addedNodes || [], function(node) {
+          // we firstly check variables
+          setting.variables.forEach(function(variable) {
+            if (!variable.done && node.getAttribute
+              && (node.getAttribute('name') === variable.name
+              ||  node.className === variable.className)) {
+              varpromise.set(variable.title, node.getAttribute(variable.attribute));
+              variable.done = true;
+            }
+          });
+          // then check whether or not we overwrite the element
+          setting.overwrites.forEach(function(overwrite) {
+            if (!overwrite.done && node.className === overwrite.className) {
+              node.style.display = 'none';
+              htmlpromise[overwrite.fileName].then(function(innerHTML) {
+                varpromise.then(function(variables) {
+                  if (!overwrite.when || variables[overwrite.when]) {
+                    node.innerHTML = Object.keys(variables).reduce(function(innerHTML, name) {
+                      return innerHTML.replace(new RegExp(`{{ *${name} *}}`, 'g'), variables[name]);
+                    }, innerHTML);
+                  }
+                  node.style.display = '';
+                });
+              });
+              overwrite.done = true;
+            }
+          });
+        });
+      });
+      // if all things done, stop observation
+      if (setting.variables.every(function(variable) { return variable.done; })
+      && setting.overwrites.every(function(overwrite) { return overwrite.done; })) {
+        this.disconnect();
+      }
+    };
+  }
+
+  // overwrite the page with the settings injected
+  function start(setting) {
+    var observer = new MutationObserver(witness(setting));
+    observer.observe(setting.target, setting.config);
+  }
+
+  // we start the process
+  start({
+    target: document,
+    config: { childList: true, subtree: true },
+    overwrites: [
+      {
+        className: 'header-nav left',
+        fileName: 'header-nav-left.html',
+        when: 'user'
+      },
+      {
+        className: 'header-nav user-nav right',
+        fileName: 'header-nav-right.html'
+      }
+    ],
+    variables: [
+      {
+        title: 'user',
+        name: 'user-login',
+        attribute: 'content'
+      },
+      {
+        title: 'avatar',
+        className: 'avatar',
+        attribute: 'src',
+        when: 'user'
+      },
+      {
+        title: 'authenticity_token',
+        name: 'authenticity_token',
+        attribute: 'value'
+      }
+    ]
+  });
+
+})();
